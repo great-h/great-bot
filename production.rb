fe47689d2bb7f4e1e97ff2da55356b84b5dd6f1a
Tomohiko Himura <eiel.hal@gmail.com>
2014-06-05 16:14:16 +0900

    add production.rb

---
+++ production.rb
@@ -0,0 +1,8 @@
+require_relative 'user_script'
+
+require 'thread'
+Thread.new do
+  app =  -> (env) { [200, {'ContentType' => 'text/html'}, ['すごい広島']] }
+  port = ENV["PORT"] || 4000
+  Thin::Server.start('0.0.0.0', port, app)
+end
