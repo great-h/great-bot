cd5276615912ce082f6807051cd89d1b307c5ee3
Tomohiko Himura <eiel.hal@gmail.com>
2014-06-05 15:47:19 +0900

    Talk コマンドを実装

---
+++ scripts/talk.rb
@@ -0,0 +1,22 @@
+logger = Ruboty.logger
+logger.debug("load: " + __FILE__)
+
+module Ruboty
+  class Talk < Handlers::Base
+    on /talk_to (.*) (.*)\z/i, name: 'talk_to', description: 'talking bot to user'
+    on /talk (.*)\z/i, name: 'talk', description: 'talking bot'
+
+    def talk_to(message)
+      Ruboty.logger.info("Handle talk_to: #{message.body}")
+      to = "@" + message[1]
+      body = message[2]
+      message.reply("#{to} #{body}")
+    end
+
+    def talk(message)
+      Ruboty.logger.info("Handle talk: #{message.body}")
+      body = message[1]
+      message.reply("#{body}")
+    end
+  end
+end
