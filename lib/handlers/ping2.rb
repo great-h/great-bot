6a897b067e8ba216f53fa86a380f94f10561654c
Tomohiko Himura <eiel.hal@gmail.com>
2014-06-18 19:55:49 +0900

    reply_to コマンドをつくってみたり

--- lib/handlers/ping2.rb
+++ lib/handlers/ping2.rb
@@ -3,6 +3,7 @@ Ruboty.handlers.delete(Ruboty::Handlers::Ping)
 module Ruboty::Handlers
   class PING2 < Base
     on /ping (.*)\z/i, name: 'ping2', description: 'sample ping'
+    on /reply_to (.*?) (.*)\z/i, name: 'reply_to', description: 'reply to meesage'
 
     def ping2(message)
       Ruboty.logger.info("Handle ping2: #{message.body}")
@@ -10,6 +11,17 @@ module Ruboty::Handlers
       to = message.original[:from]
       Ruboty.escape(body)
       message.reply("@#{to} #{body}")
+    end
+
+    def reply_to(message)
+      status_id = message[1]
+      body = message[2]
+      adapter = @robot.send(:adapter)
+      if adapter.class == Ruboty::Adapters::Twitter
+        tweet = @robot.send(:client).status(status_id)
+        name = tweet.user.screen_name
+      end
+      @robot.say(body: "#{name} #{body}", in_reply_to_status_id: status_id)
     end
   end
 end
