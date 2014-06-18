3e502672142aba82997b0244792103860dc4fef2
Tomohiko Himura <eiel.hal@gmail.com>
2014-06-18 16:50:54 +0900

    語尾が変わるようにしてみた。

--- lib/handlers/event.rb
+++ lib/handlers/event.rb
@@ -6,6 +6,11 @@ module Ruboty::Handlers
   class Event < Base
     on /(.*)(イベント|event)/i, name: 'event', description: 'next event infomation'
     API_URL = 'http://great-h.github.io/event.json'
+    SUFFIX_PATTERN = %w{ですよー ですー だよ じゃけぇ よ ですにゅ ですわ}
+
+    def suffix
+      SUFFIX_PATTERN.shuffle.first
+    end
 
     def event(message)
       Ruboty.logger.info("Handle event: #{message.body}")
@@ -15,7 +20,7 @@ module Ruboty::Handlers
       url = 'http://great-h.github.io/events/event-%03d.html' % no
       date = DateTime.parse(attributes['datetime']).strftime('%m月%d日')
       place = attributes['place']
-      body = "#すごい広島 #{no} は#{date}ですー。 場所は #{place} だよ。 #{url}"
+      body = "#すごい広島 #{no} は#{date}#{suffix}。 場所は #{place} #{suffix}。 #{url}"
       message.reply(".@#{to} #{body}")
     end
   end
