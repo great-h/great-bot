12523320eb848a30c04a92991edaf777850d6e55
北䑓如法 <algebraicallyClosedField@gmail.com>
2014-08-08 10:31:50 +0900

    (retweet) extract urls from info of tweet
    t.co な URL をツイートのURL短縮情報から取り出すように

--- lib/handlers/retweet.rb
+++ lib/handlers/retweet.rb
@@ -4,10 +4,7 @@ module Ruboty::Handlers
 
     def retweet(message)
       Ruboty.logger.info("Handle retweet: #{message.body}")
-      tweet_id = message[2]
-      if /^(http|https):\/\/(.*)\/(\d+)$/ =~ tweet_id
-        tweet_id = $3
-      end
+      tweet_to_be_retweeted = extract_tweet(message)
       to = message.original[:from]
       adapter = @robot.send(:adapter)
 
@@ -15,11 +12,23 @@ module Ruboty::Handlers
       if adapter.class == adapter_klass
         client = adapter.send(:client)
         begin
-          client.retweet tweet_id
+          client.retweet tweet_to_be_retweeted
         rescue Twitter::Error::NotFound
-          message.reply("@#{to} tweet ID #{tweet_id} を RT しようとして失敗しました。")
+          message.reply("@#{to} tweet #{tweet_to_be_retweeted} を RT しようとして失敗しました。")
         end
       end
+    end
+
+    def extract_url(s, tweet)
+      return s unless tweet.urls?
+      tweet.urls.each do |uri|
+        s.gsub!(uri.url.to_s, uri.expanded_url.to_s)
+      end
+      s
+    end
+
+    def extract_tweet(message)
+      extract_url(message[2], message.original[:tweet])
     end
   end
 end
